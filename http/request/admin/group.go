f5aadec6c03ed2f4c91da99903cf09798cd7d14f
ljw <84855512@qq.com>
2024-10-20 20:15:12 +0800

    fix group create type

--- http/request/admin/group.go
+++ http/request/admin/group.go
@@ -5,11 +5,13 @@ import "Gwen/model"
 type GroupForm struct {
 	Id   uint   `json:"id"`
 	Name string `json:"name" validate:"required"`
+	Type int    `json:"type"`
 }
 
 func (gf *GroupForm) FromGroup(group *model.Group) *GroupForm {
 	gf.Id = group.Id
 	gf.Name = group.Name
+	gf.Type = group.Type
 	return gf
 }
 
@@ -17,5 +19,6 @@ func (gf *GroupForm) ToGroup() *model.Group {
 	group := &model.Group{}
 	group.Id = gf.Id
 	group.Name = gf.Name
+	group.Type = gf.Type
 	return group
 }
