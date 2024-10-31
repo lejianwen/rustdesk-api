b36aa6f917212145fa36d6f989b7a17e07ae1018
Tao Chen <iamtaochen@outlook.com>
2024-10-31 16:22:42 +0800

    add IsPasswordEmpty...

--- service/user.go
+++ service/user.go
@@ -304,3 +304,27 @@ func (us *UserService) FindLatestUserIdFromLoginLogByUuid(uuid string) uint {
 	global.DB.Where("uuid = ?", uuid).Order("id desc").First(llog)
 	return llog.UserId
 }
+
+// IsPasswordEmptyById 根据用户id判断密码是否为空，主要用于第三方登录的自动注册
+func (us *UserService) IsPasswordEmptyById(id uint) bool {
+	u := &model.User{}
+	if global.DB.Where("id = ?", id).First(u).Error != nil {
+		return false
+	}
+	return u.Password == ""
+}
+
+// IsPasswordEmptyByUsername 根据用户id判断密码是否为空，主要用于第三方登录的自动注册
+func (us *UserService) IsPasswordEmptyByUsername(username string) bool {
+	u := &model.User{}
+	if global.DB.Where("username = ?", username).First(u).Error != nil {
+		return false
+	}
+	return u.Password == ""
+}
+
+// IsPasswordEmptyByUser 判断密码是否为空，主要用于第三方登录的自动注册
+func (us *UserService) IsPasswordEmptyByUser(u *model.User) bool {
+	return us.IsPasswordEmptyById(u.Id)
+}
+
