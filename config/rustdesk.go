737fe749de93895f00650cec5bf16d45a2bc87ce
ljw <84855512@qq.com>
2024-11-01 14:56:11 +0800

    load key from file

--- config/rustdesk.go
+++ config/rustdesk.go
@@ -1,9 +1,30 @@
 package config
 
+import (
+	"os"
+)
+
 type Rustdesk struct {
 	IdServer    string `mapstructure:"id-server"`
 	RelayServer string `mapstructure:"relay-server"`
 	ApiServer   string `mapstructure:"api-server"`
 	Key         string `mapstructure:"key"`
+	KeyFile     string `mapstructure:"key-file"`
 	Personal    int    `mapstructure:"personal"`
+}
+
+func LoadKeyFile(rustdesk *Rustdesk) {
+	// Load key file
+	if rustdesk.Key != "" {
+		return
+	}
+	if rustdesk.KeyFile != "" {
+		// Load key from file
+		b, err := os.ReadFile(rustdesk.KeyFile)
+		if err != nil {
+			return
+		}
+		rustdesk.Key = string(b)
+		return
+	}
 }
