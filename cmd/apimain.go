793614841a9ce971b45e964fe9f63038a8b8be7d
ljw <84855512@qq.com>
2024-11-05 21:03:05 +0800

    fix migrate

--- cmd/apimain.go
+++ cmd/apimain.go
@@ -150,7 +150,7 @@ func DatabaseAutoUpdate() {
 		if v.Version < 245 {
 			//oauths 表的 oauth_type 字段设置为 op同样的值
 			db.Exec("update oauths set oauth_type = op")
-			db.Exec("update oauths set issuer = 'https://accounts.google.com' where op = 'google' and issuer = ''")
+			db.Exec("update oauths set issuer = 'https://accounts.google.com' where op = 'google'")
 			db.Exec("update user_thirds set oauth_type = third_type, op = third_type")
 			//通过email迁移旧的google授权
 			uts := make([]model.UserThird, 0)
