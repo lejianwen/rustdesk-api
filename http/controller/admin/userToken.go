a465888b31778aa338d4f6275c45e06388f3e67a
lejianwen <84855512@qq.com>
2024-12-06 10:20:21 +0800

    up username length to 32 #70

--- http/controller/admin/userToken.go
+++ http/controller/admin/userToken.go
@@ -81,3 +81,44 @@ func (ct *UserToken) Delete(c *gin.Context) {
 	}
 	response.Fail(c, 101, response.TranslateMsg(c, "ItemNotFound"))
 }
+
+// BatchDelete 批量删除
+// @Tags 登录凭证
+// @Summary 登录凭证批量删除
+// @Description 登录凭证批量删除
+// @Accept  json
+// @Produce  json
+// @Param body body model.UserToken true "登录凭证信息"
+// @Success 200 {object} response.Response
+// @Failure 500 {object} response.Response
+// @Router /admin/user_token/delete [post]
+// @Security token
+func (ct *UserToken) BatchDelete(c *gin.Context) {
+	f := &model.UserToken{}
+	if err := c.ShouldBindJSON(f); err != nil {
+		response.Fail(c, 101, response.TranslateMsg(c, "ParamsError")+err.Error())
+		return
+	}
+	id := f.Id
+	errList := global.Validator.ValidVar(c, id, "required,gt=0")
+	if len(errList) > 0 {
+		response.Fail(c, 101, errList[0])
+		return
+	}
+	l := service.AllService.UserService.TokenInfoById(f.Id)
+	u := service.AllService.UserService.CurUser(c)
+	if !service.AllService.UserService.IsAdmin(u) && l.UserId != u.Id {
+		response.Fail(c, 101, response.TranslateMsg(c, "NoAccess"))
+		return
+	}
+	if l.Id > 0 {
+		err := service.AllService.UserService.DeleteToken(l)
+		if err == nil {
+			response.Success(c, nil)
+			return
+		}
+		response.Fail(c, 101, err.Error())
+		return
+	}
+	response.Fail(c, 101, response.TranslateMsg(c, "ItemNotFound"))
+}
