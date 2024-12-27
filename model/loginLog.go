e8b2425222de933706c0732233730b429405a4c7
lejianwen <84855512@qq.com>
2024-12-27 19:25:59 +0800

    feat(admin): Add My Login log

--- model/loginLog.go
+++ model/loginLog.go
@@ -4,12 +4,13 @@ type LoginLog struct {
 	IdModel
 	UserId      uint   `json:"user_id" gorm:"default:0;not null;"`
 	Client      string `json:"client"` //webadmin,webclient,app,
-	DeviceId	string `json:"device_id"`
+	DeviceId    string `json:"device_id"`
 	Uuid        string `json:"uuid"`
 	Ip          string `json:"ip"`
 	Type        string `json:"type"`     //account,oauth
 	Platform    string `json:"platform"` //windows,linux,mac,android,ios
 	UserTokenId uint   `json:"user_token_id" gorm:"default:0;not null;"`
+	IsDeleted   uint   `json:"is_deleted" gorm:"default:0;not null;"`
 	TimeModel
 }
 
@@ -22,6 +23,11 @@ const (
 const (
 	LoginLogTypeAccount = "account"
 	LoginLogTypeOauth   = "oauth"
+)
+
+const (
+	IsDeletedNo  = 0
+	IsDeletedYes = 1
 )
 
 type LoginLogList struct {
