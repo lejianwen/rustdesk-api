5011e2b7c172578428015ece411d602061975bad
lejianwen <84855512@qq.com>
2025-02-07 17:51:42 +0800

    feat: Web sso env (#125)

--- config/config.go
+++ config/config.go
@@ -17,6 +17,7 @@ type App struct {
 	Register    bool `mapstructure:"register"`
 	ShowSwagger int  `mapstructure:"show-swagger"`
 	TokenExpire int  `mapstructure:"token-expire"`
+	WebSso      bool `mapstructure:"web-sso"`
 }
 type Admin struct {
 	Title     string `mapstructure:"title"`
