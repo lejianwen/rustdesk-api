878d5fd27cb89b1189af29a9b4c4c6c7c29b04f4
lejianwen <84855512@qq.com>
2025-02-12 16:25:04 +0800

    style: Remove generate

--- generate_api.go
+++
@@ -1,5 +0,0 @@
-package Gwen
-
-//go:generate swag init -g cmd/apimain.go --output docs/api --instanceName api --exclude http/controller/admin
-//go:generate swag init -g cmd/apimain.go --output docs/admin --instanceName admin --exclude http/controller/api
-//go:generate go run cmd/apimain.go
