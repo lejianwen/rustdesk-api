c52706e62191e79e17a26d1ddfc25daa79b668ec
k3-cat <k3-cat@outlook.com>
2025-07-04 16:30:07 +1000

    feat: Improve oauth redirect (#303)
    * fix: redirects after oauth can potentially misalign with server's actually hostname
    
    * feat: remove `RedirectURL` from oauth config, as it should checked by provider rather than client
    
    * feat: align oauth endpoint with the hostname in requests

--- config/oauth.go
+++ config/oauth.go
@@ -3,24 +3,20 @@ package config
 type GithubOauth struct {
 	ClientId     string `mapstructure:"client-id"`
 	ClientSecret string `mapstructure:"client-secret"`
-	RedirectUrl  string `mapstructure:"redirect-url"`
 }
 
 type GoogleOauth struct {
 	ClientId     string `mapstructure:"client-id"`
 	ClientSecret string `mapstructure:"client-secret"`
-	RedirectUrl  string `mapstructure:"redirect-url"`
 }
 
 type OidcOauth struct {
 	Issuer       string `mapstructure:"issuer"`
 	ClientId     string `mapstructure:"client-id"`
 	ClientSecret string `mapstructure:"client-secret"`
-	RedirectUrl  string `mapstructure:"redirect-url"`
 }
 
 type LinuxdoOauth struct {
 	ClientId     string `mapstructure:"client-id"`
 	ClientSecret string `mapstructure:"client-secret"`
-	RedirectUrl  string `mapstructure:"redirect-url"`
 }
